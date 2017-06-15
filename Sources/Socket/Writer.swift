4afb1d8e3a7538b061319b250ba6f78d22ffe1da
Adam Sharp <adam@sharplet.me>
2017-06-15 14:10:57 -0400

    Simplify Writer by modelling writes as a queue of buffers
    There was a weird issue that seemingly resulted from appending bytes to
    a single buffer rather than using discrete buffers. We now use a queue
    of buffers instead, which should also hopefully avoid excessive moves or
    copies.

--- Sources/Socket/Writer.swift
+++ Sources/Socket/Writer.swift
@@ -4,40 +4,21 @@ import ReactiveSwift
 import Result
 
 final class Writer {
-  private enum Event {
-    case writeFinished
-    case writeFailed(SocketError)
-  }
-
   private var buffer = Buffer()
   private let source: DispatchSourceWrite
-  private let (events, observer) = Signal<Event, NoError>.pipe()
 
   func write(_ data: Data) -> SignalProducer<Never, SocketError> {
-    return SignalProducer { [buffer, events, source] observer, lifetime in
+    return SignalProducer { observer, lifetime in
       guard data.count > 0 else {
         observer.sendCompleted()
         return
       }
 
-      if buffer.isEmpty {
-        defer { source.resume() }
-      }
+      let disposable = self.buffer
+        .write(data)
+        .start(observer)
 
-      buffer.append(data)
-
-      let disposable = events.observe { event in
-        switch event {
-        case .value(.writeFinished), .completed:
-          observer.sendCompleted()
-        case let .value(.writeFailed(error)):
-          observer.send(error: error)
-        case .interrupted:
-          observer.sendInterrupted()
-        }
-      }
-
-      lifetime.observeEnded { disposable?.dispose() }
+      lifetime.observeEnded(disposable.dispose)
     }
   }
 
@@ -48,88 +29,78 @@ final class Writer {
 
     source.setCancelHandler { _ = connection }
 
-    source.setEventHandler { [buffer, observer, source] in
-      assert(buffer.count > 0)
-
-      let available = Int(source.data)
-      let limit = min(available, buffer.count)
-
-      let result = buffer.write(upTo: limit, on: connection.fileDescriptor)
-
-      switch result {
-      case let .failed(error):
-        observer.send(value: .writeFailed(error))
-      case .bytesRemaining:
-        break
-      case .complete:
-        source.suspend()
-        observer.send(value: .writeFinished)
-      case .finalized:
-        source.cancel()
-        observer.sendCompleted()
-      }
+    source.setEventHandler { [buffer, source] in
+      guard buffer.hasData else { return }
+      buffer.write(upTo: Int(source.data), on: connection.fileDescriptor)
     }
+
+    source.resume()
   }
 
   deinit {
-    if buffer.isEmpty {
-      source.cancel()
-      source.resume()
-    } else {
-      buffer.finalize()
-    }
+    source.cancel()
   }
 }
 
 private final class Buffer {
   enum WriteResult {
-    case bytesRemaining
+    case partial
     case complete
-    case finalized
     case failed(SocketError)
   }
 
-  private var data = Data()
-  private var shouldFinalize = false
+  private let (events, observer) = Signal<(Int, WriteResult), NoError>.pipe()
+  private var counter = 0
+  private var queue: [(id: Int, data: Data)] = []
 
-  var count: Int {
-    return data.count
+  var hasData: Bool {
+    return queue.count > 0
   }
 
-  var isEmpty: Bool {
-    return data.isEmpty
+  func write(_ data: Data) -> SignalProducer<Never, SocketError> {
+    return SignalProducer { observer, lifetime in
+      let id = self.counter
+      self.counter += 1
+      self.queue.append((id, data))
+
+      let disposable = self.events.observe { event in
+        switch event {
+        case let .value((eventId, .complete)) where eventId == id:
+          observer.sendCompleted()
+
+        case let .value((eventId, .failed(error))) where eventId == id:
+          observer.send(error: error)
+
+        default:
+          break
+        }
+      }
+
+      lifetime.observeEnded { disposable?.dispose() }
+    }
   }
 
-  func append(_ data: Data) {
-    self.data.append(data)
-  }
+  func write(upTo available: Int, on fileDescriptor: Int32) {
+    guard queue.count > 0 else { preconditionFailure("Tried to write with no enqueued data") }
 
-  func finalize() {
-    precondition(shouldFinalize == false, "buffer finalized twice")
-    shouldFinalize = true
-  }
+    let id = queue[0].id
+    let count = queue[0].data.count
+    let limit = min(available, count)
 
-  func write(upTo count: Int, on fileDescriptor: Int32) -> WriteResult {
-    let written = data.withUnsafeBytes { buffer in
-      Darwin.write(fileDescriptor, buffer, count)
+    let written = queue[0].data.withUnsafeBytes { buffer in
+      Darwin.write(fileDescriptor, buffer, limit)
     }
 
     switch written {
     case ..<0:
-      return .failed(.make("Socket write failed"))
+      observer.send(value: (id, .failed(.make("Socket write failed"))))
     default:
-      if written > 0 {
-        data.removeFirst(written)
-      }
-
-      if isEmpty {
-        if shouldFinalize {
-          return .finalized
-        } else {
-          return .complete
-        }
+      if written == count {
+        queue.removeFirst()
+        observer.send(value: (id, .complete))
       } else {
-        return .bytesRemaining
+        queue[0].data.removeFirst(written)
+        observer.send(value: (id, .partial))
       }
     }
   }
